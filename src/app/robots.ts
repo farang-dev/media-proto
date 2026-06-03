6d39c30b306499ab60fd2474e7f4f809967fc8e3
Masafumi Nozawa <masafumi.nozawa.q@gmail.com>
2026-06-04 00:11:36 +0300

    mod: UI and added events and content

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ['/auth', '/api/'],
       },
     ],
-    sitemap: 'https://www.oshi-hos.xyz/sitemap.xml',
+    sitemap: 'https://oshi-hos.xyz/sitemap.xml',
   };
 }
